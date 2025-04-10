4e29a09dac3d6d8385a54ae8f8b60137151b27cb
MathisDelobel <delobelmathis@gmail.com>
2025-04-10 12:29:47 +0200

    Fixtures api-journey

--- api-journey/data/init-db.sql
+++ api-journey/data/init-db.sql
@@ -3,25 +3,21 @@ BEGIN ;
 DROP TABLE IF EXISTS "journey" ;
 
 CREATE TABLE journey (
-  "id" INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "departure" TEXT NOT NULL,
   "destination" TEXT NOT NULL,
-  "date" DATETIME NOT NULL,
+  "date" TIMESTAMPTZ NOT NULL,
   "seatsAvailable" INT NOT NULL,
   "price" FLOAT NOT NULL,
   "time" INT NOT NULL,
-  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  "createdAt" TIMESTAMPTZ NOT NULL default(now()),
+  "updatedAt" TIMESTAMPTZ
   
   -- Ajout des contraintes de validation
-  CONSTRAINT chk_date CHECK (date > NOW()),
-  CONSTRAINT chk_seats CHECK (seatsAvailable >= 0),
-  CONSTRAINT chk_price CHECK (price >= 0),
-  CONSTRAINT chk_time CHECK (time >= 0)
+  CONSTRAINT chk_date CHECK ("date" > NOW()),
+  CONSTRAINT chk_seats CHECK ("seatsAvailable" >= 0),
+  CONSTRAINT chk_price CHECK ("price" >= 0),
+  CONSTRAINT chk_time CHECK ("time" >= 0)
 );
-
--- Création des index pour optimiser les recherches
-CREATE INDEX idx_departure_destination ON journeys (departure, destination);
-CREATE INDEX idx_date ON journeys (date);
 
 COMMIT;
--- api-journey/models/db.ts
+++ api-journey/models/db.ts
@@ -4,9 +4,7 @@ if (!process.env.PG_URL) {
 	throw new Error("PG_URL environment variable is missing");
 }
 
-const sequelize = new Sequelize(process.env.PG_URL, {
-	logging: false,
-});
+const sequelize = new Sequelize(process.env.PG_URL, {});
 
 async function testConnection() {
 	try {
